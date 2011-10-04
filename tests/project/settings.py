707a03599ddd4bb06c6df99624f01c8cd0b1e75c
Michael Thornhill <michael.thornhill@gmail.com>
2011-10-04 10:13:01 +0100

    use postgres for tests

--- tests/project/settings.py
+++ tests/project/settings.py
@@ -16,8 +16,12 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'cms.sqlite',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'cms',                      # Or path to database file if using sqlite3.
+        'USER': 'cms',                      # Not used with sqlite3.
+        'PASSWORD': 'password',                  # Not used with sqlite3.
+        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
 }
 
